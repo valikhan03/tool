8e238326bd4d01f997265f80f8090df906df7ed4
valikhan03 <valikhan.eltaev@gmail.com>
2023-04-10 21:55:58 +0600

    fix

--- models/charge.go
+++ models/charge.go
@@ -1,12 +1,12 @@
 package tool_models
 
 type Charge struct {
-	ID          int     `json:"id" db:"id"`
-	UserID      int     `json:"user_id" db:"user_id"`
-	ProductType int     `json:"product_type" db:"product_type"`
-	ProductID   int     `json:"product_id" db:"product_id"`
-	ProductName string     `json:"product_name" db:"product_name"`
-	Amount      int64 `json:"amount" db:"amount"`
-	Currency    string  `json:"currency" db:"currency"`
-	PayStatus   int     `json:"p_is_payed" db:"p_is_payed"`
+	ID          int    `json:"id" db:"id"`
+	UserID      int    `json:"user_id" db:"user_id"`
+	ProductType int    `json:"product_type" db:"product_type"`
+	ProductID   int    `json:"product_id" db:"product_id"`
+	ProductName string `json:"product_name" db:"product_name"`
+	Amount      int64  `json:"amount" db:"amount"`
+	Currency    string `json:"currency" db:"currency"`
+	PayStatus   int    `json:"p_is_payed" db:"p_is_payed"`
 }
