8b9b372a864f06978473080fad0f0cf7a79053df
valikhan03 <valikhan.eltaev@gmail.com>
2023-04-10 21:54:01 +0600

    charge model fix

--- models/charge.go
+++ models/charge.go
@@ -5,8 +5,8 @@ type Charge struct {
 	UserID      int     `json:"user_id" db:"user_id"`
 	ProductType int     `json:"product_type" db:"product_type"`
 	ProductID   int     `json:"product_id" db:"product_id"`
-	ProductName int     `json:"product_name" db:"product_name"`
-	Amount      float64 `json:"amount" db:"amount"`
+	ProductName string     `json:"product_name" db:"product_name"`
+	Amount      int64 `json:"amount" db:"amount"`
 	Currency    string  `json:"currency" db:"currency"`
 	PayStatus   int     `json:"p_is_payed" db:"p_is_payed"`
 }
